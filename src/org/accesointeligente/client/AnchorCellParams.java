f3a84e523c6408cb4ad7c95553d874c269c9a887
José González Quintana <jgonzalezq@votainteligente.cl>
2011-01-04 12:24:11 -0300

    Makes request title clickable on request list
    Adds custom AnchorCell for CellTable
    
    Change-Id: Idc6b2cfbcdd545ca1ae4cfed86e187ee0536089b

---
+++ src/org/accesointeligente/client/AnchorCellParams.java
@@ -0,0 +1,28 @@
+package org.accesointeligente.client;
+
+public class AnchorCellParams {
+	private String url;
+	private String title;
+	private String value;
+
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getValue() {
+		return value;
+	}
+	public void setValue(String value) {
+		this.value = value;
+	}
+}
