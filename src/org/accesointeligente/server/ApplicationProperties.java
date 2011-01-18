de9f1c791d185f6421808035331bc7e659f9d751
Ricardo Arcos <rarcos@votainteligente.cl>
2011-01-18 12:09:08 -0300

    Add properties handling
    Change-Id: I69f4f9d43fb65684ea584ccbd5782d3fd1057e4c

---
+++ src/org/accesointeligente/server/ApplicationProperties.java
@@ -0,0 +1,36 @@
+package org.accesointeligente.server;
+
+import java.io.FileInputStream;
+import java.util.Properties;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+public class ApplicationProperties implements ServletContextListener {
+	private static Properties properties;
+
+	@Override
+	public void contextDestroyed(ServletContextEvent event) {
+		properties = null;
+	}
+
+	@Override
+	public void contextInitialized(ServletContextEvent event) {
+		try {
+			properties = new Properties();
+			properties.load(new FileInputStream(event.getServletContext().getRealPath("/WEB-INF/accesointeligente.properties")));
+		} catch (Throwable ex) {
+			ex.printStackTrace(System.err);
+		}
+	}
+
+	public static String getProperty(String key) {
+		String property = properties.getProperty(key);
+
+		if (property != null && property.length() == 0) {
+			property = null;
+		}
+
+		return property;
+	}
+}
