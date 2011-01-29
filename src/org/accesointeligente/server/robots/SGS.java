6dfb1890a3b8a1c6226df8e79fe375772b73ca8b
Ricardo Arcos <rarcos@votainteligente.cl>
2011-01-28 21:12:54 -0300

    Add production configuration parameters
    Change-Id: Ib781620f9da0952228a03797b58c83c0202d4c6e

--- src/org/accesointeligente/server/robots/SGS.java
+++ src/org/accesointeligente/server/robots/SGS.java
@@ -33,6 +33,9 @@ public class SGS extends Robot {
 		client = new DefaultHttpClient();
 		HttpProtocolParams.setUserAgent(client.getParams(), "Mozilla/5.0 (X11; U; Linux x86_64; es-CL; rv:1.9.2.12) Gecko/20101027 Ubuntu/10.10 (maverick) Firefox/3.6.12");
 		cleaner = new HtmlCleaner();
+		// FIXME: these must be configurable
+		setUsername("info@accesointeligente.org");
+		setPassword("holanda895:AI");
 	}
 
 	@Override
