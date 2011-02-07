79c315d73da95296d76b1530e27e3cb9911f49ea
José González Quintana <jgonzalezq@votainteligente.cl>
2011-02-07 15:22:43 -0300

    Activity: Implements hashCode and equals methods
    Change-Id: I199aea828e5a0a902750c792a8a1c4c75627a021

--- src/org/accesointeligente/model/Activity.java
+++ src/org/accesointeligente/model/Activity.java
@@ -3,7 +3,6 @@ package org.accesointeligente.model;
 import net.sf.gilead.pojo.gwt.LightEntity;
 
 public class Activity extends LightEntity {
-	private static final long serialVersionUID = -7949584853990558020L;
 
 	private Integer id;
 	private String name;
@@ -31,5 +30,27 @@ public class Activity extends LightEntity {
 
 	public Boolean isPerson() {
 		return person;
+	}
+
+	@Override
+	public int hashCode() {
+		return this.id.hashCode();
+	}
+
+	@Override
+	public boolean equals(Object object) {
+		if (object == null) {
+			return false;
+		}
+
+		if (id == null) {
+			return false;
+		}
+
+		if (!(object instanceof Activity)) {
+			return false;
+		}
+
+		return id.equals(((Activity) object).getId());
 	}
 }
