dc11eb74facdc54fbfafbbe4f18790c4c1affa47
José González Quintana <jgonzalezq@votainteligente.cl>
2011-02-11 16:12:22 -0300

    Emailer: Fixes SMTP properties, Auth and Charset
    Change-Id: I7c6b1b18e587edb31b8ddcf30bfe8b273e298c16

--- src/org/accesointeligente/server/Emailer.java
+++ src/org/accesointeligente/server/Emailer.java
@@ -1,6 +1,7 @@
 package org.accesointeligente.server;
 
 
+import java.util.Date;
 import java.util.Properties;
 
 import javax.mail.*;
@@ -49,24 +50,37 @@ public class Emailer {
 	}
 
 	public Boolean connectAndSend() {
-		if (ApplicationProperties.getProperty("email.server") == null || ApplicationProperties.getProperty("email.user") == null || ApplicationProperties.getProperty("email.password") == null) {
+		if (ApplicationProperties.getProperty("email.smtp") == null || ApplicationProperties.getProperty("email.user") == null || ApplicationProperties.getProperty("email.password") == null) {
 			System.err.println("Emailer: No estan definidas las propiedades!");
 			return false;
 		}
+
+		props.put("mail.smtp.host", ApplicationProperties.getProperty("email.smtp"));
+		props.put("mail.smtp.port", "465");
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.socketFactory.port", "465");
+		props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+		props.put("mail.smtp.socketFactory.fallback", "false");
 
 		if (getRecipient() == null || getSubject() == null || getBody() == null) {
 			System.err.println("Emailer: No estan definidas las partes del correo");
 			return false;
 		}
 
-		Session session = Session.getDefaultInstance(props, null);
-		MimeMessage message = new MimeMessage(session);
+		Session session = Session.getDefaultInstance(props, new Authenticator() {
+			@Override
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(ApplicationProperties.getProperty("email.user"), ApplicationProperties.getProperty("email.password"));
+			}
+		});
 
 		try {
+			MimeMessage message = new MimeMessage(session);
 			message.setFrom(new InternetAddress(ApplicationProperties.getProperty("email.user")));
 			message.addRecipient(Message.RecipientType.TO, new InternetAddress(getRecipient()));
 			message.setSubject(getSubject());
-			message.setContent(getBody(), "text/html");
+			message.setContent(getBody(), "text/html;charset=UTF-8");
+			message.setSentDate(new Date());
 			Transport.send(message);
 			return true;
 		} catch (MessagingException ex) {
