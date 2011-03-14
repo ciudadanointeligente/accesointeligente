8dbad37f80edb773ea609a4cfc867cace4c567f2
José González Quintana <jgonzalezq@votainteligente.cl>
2011-03-14 18:11:42 -0300

    Implements Validator
    Change-Id: Ifb18b977c13a6a4a15de63157828a46fbb6182f8

---
+++ src/org/accesointeligente/shared/Validator.java
@@ -0,0 +1,34 @@
+/**
+ * Acceso Inteligente
+ *
+ * Copyright (C) 2010-2011 Fundación Ciudadano Inteligente
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.accesointeligente.shared;
+
+public class Validator {
+
+	private static final String EMAIL_PATTERN = "^[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+
+	/**
+	 * Validate emailAddress with regular expression
+	 *
+	 * @param emailAddress email address for validation
+	 * @return true valid email address, false invalid email address
+	 */
+	public static boolean validateEmail(String emailAddress) {
+		return emailAddress.matches(EMAIL_PATTERN);
+	}
+}
