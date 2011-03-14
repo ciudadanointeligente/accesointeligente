83c65fbd06fbfc39deff894527cdf0d39bc18314
José González Quintana <jgonzalezq@votainteligente.cl>
2011-03-14 17:58:35 -0300

    Adds contact to the model
    Change-Id: Ib4938c8ce9546ee7537ce5903621ba259c6c4031

---
+++ src/org/accesointeligente/model/Contact.java
@@ -0,0 +1,44 @@
+package org.accesointeligente.model;
+
+import net.sf.gilead.pojo.gwt.LightEntity;
+
+public class Contact extends LightEntity {
+
+	private String name;
+	private String email;
+	private String subject;
+	private String message;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+}
