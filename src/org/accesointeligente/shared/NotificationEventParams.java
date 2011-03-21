054204aedee19c8a9af71d77afb9ea653631baa3
José González Quintana <jgonzalezq@votainteligente.cl>
2011-03-21 12:34:47 -0300

    NotiticationEventParams: adds duration attribute and constants
    Change-Id: If495c1569c821b5f59ad44b0117c2f4a6d846a0c

--- src/org/accesointeligente/shared/NotificationEventParams.java
+++ src/org/accesointeligente/shared/NotificationEventParams.java
@@ -22,6 +22,12 @@ public class NotificationEventParams {
 
 	private NotificationEventType type;
 	private String message;
+	private Integer duration;
+
+	public static final Integer DURATION_SHORT = 10000;
+	public static final Integer DURATION_NORMAL = 15000;
+	public static final Integer DURATION_LONG = 20000;
+	public static final Integer DURATION_PERMANENT = 0;
 
 	public NotificationEventType getType() {
 		return type;
@@ -37,5 +43,13 @@ public class NotificationEventParams {
 
 	public void setMessage(String message) {
 		this.message = message;
+	}
+
+	public Integer getDuration() {
+		return duration;
+	}
+
+	public void setDuration(Integer duration) {
+		this.duration = duration;
 	}
 }
