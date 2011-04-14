8a000e889daa5f0ce0c696731e03d533ad92a309
José González Quintana <jgonzalezq@votainteligente.cl>
2011-04-14 19:39:15 -0300

    Implements Terms And Conditions Presenter and View
    Change-Id: I9771a39df1bf3ebf97311a6f3590a2944a27e688

---
+++ src/org/accesointeligente/client/presenters/TermsAndConditionsPresenter.java
@@ -0,0 +1,61 @@
+/**
+ * Acceso Inteligente
+ *
+ * Copyright (C) 2010-2011 Fundación Ciudadano Inteligente
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.accesointeligente.client.presenters;
+
+import org.accesointeligente.client.AppController;
+
+import net.customware.gwt.presenter.client.EventBus;
+import net.customware.gwt.presenter.client.widget.WidgetDisplay;
+
+import com.google.gwt.user.client.History;
+import com.google.inject.Inject;
+
+public class TermsAndConditionsPresenter extends CustomWidgetPresenter<TermsAndConditionsPresenter.Display> implements TermsAndConditionsPresenterIface {
+	public interface Display extends WidgetDisplay {
+		void setPresenter(TermsAndConditionsPresenterIface presenter);
+	}
+
+	@Inject
+	public TermsAndConditionsPresenter(Display display, EventBus eventBus) {
+		super(display, eventBus);
+		bind();
+	}
+
+	@Override
+	public void setup() {
+	}
+
+	@Override
+	protected void onBind() {
+		display.setPresenter(this);
+	}
+
+	@Override
+	protected void onUnbind() {
+	}
+
+	@Override
+	protected void onRevealDisplay() {
+	}
+
+	@Override
+	public void close() {
+		History.newItem(AppController.getPreviousHistoryToken());
+	}
+}
