71b38f83db33740b4b04cae7dba117bdd4e5536f
Jose Gonzalez Quintana <jgonzalezq@votainteligente.cl>
2011-09-08 15:45:41 -0300

    ShareThis: Fixes facebook widget width
    Change-Id: I294fe36cfade3d72589e0993c5b842ba8415e662

--- src/org/accesointeligente/client/widgets/ShareThis.java
+++ src/org/accesointeligente/client/widgets/ShareThis.java
@@ -196,7 +196,7 @@ public class ShareThis extends Composite {
 		html += "layout=\"" + align.getFacebookPropertyName() + "\" ";
 		html += "show_faces=\"false\" ";
 		html += "send=\"true\" ";
-		html += "width=\"200\">";
+		html += "width=\"80\">";
 		html += "</fb:like>";
 
 		setHtmlFacebook(new HTML(html));
