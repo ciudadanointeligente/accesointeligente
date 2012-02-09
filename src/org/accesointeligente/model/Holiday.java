cfbc7194563a493e2c5939522c629dbb99ce7f73
Juan Pablo Perez T <jpperez@ciudadanointeligente.cl>
2012-02-09 12:59:22 -0300

    Adds Holiday model
    Change-Id: I5f7c58f9a216aa5122db1af28c25ecb5fa88c42d

---
+++ src/org/accesointeligente/model/Holiday.java
@@ -0,0 +1,76 @@
+/**
+ * Acceso Inteligente
+ *
+ * Copyright (C) 2010-2012 Fundación Ciudadano Inteligente
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.accesointeligente.model;
+
+import java.util.Date;
+
+import net.sf.gilead.pojo.gwt.LightEntity;
+
+public class Holiday extends LightEntity {
+
+	private Integer id;
+	private Date date;
+	private String description;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	@Override
+	public int hashCode() {
+		return this.id.hashCode();
+	}
+
+	@Override
+	public boolean equals(Object object) {
+		if (object == null) {
+			return false;
+		}
+
+		if (id == null) {
+			return false;
+		}
+
+		if (!(object instanceof Holiday)) {
+			return false;
+		}
+
+		return id.equals(((Holiday) object).getId());
+	}
+}
